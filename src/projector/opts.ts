34586a67b5bc989075ec9526181ac04414aeb551
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-03-18 16:14:28 -0400

    Using the cli app!  fin

--- src/projector/opts.ts
+++ src/projector/opts.ts
@@ -1,26 +1,28 @@
-import cli from "command-line-args"
+import * as cli from "command-line-args";
 
 export type Opts = {
-    args?: string[],
-    pwd?: string, //present working directory
-    config?: string,
-}
+  args?: string[];
+  pwd?: string; //present working directory
+  config?: string;
+};
 
 export default function getOpts(): Opts {
-    return cli([{
-        name: "args",
-        defaultOption: true,
-        multiple: true,
-        type: String
+  return cli([
+    {
+      name: "args",
+      defaultOption: true,
+      multiple: true,
+      type: String,
     },
     {
-        name: "config",
-        alias: "c",
-        type: String
+      name: "config",
+      alias: "c",
+      type: String,
     },
     {
-        name: "pwd",
-        alias: "p",
-        type: String
-    }]) as Opts;
+      name: "pwd",
+      alias: "p",
+      type: String,
+    },
+  ]) as Opts;
 }
